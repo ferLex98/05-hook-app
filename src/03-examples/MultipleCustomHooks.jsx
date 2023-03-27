f4684d3a1f2356ca2347c5c61081f8a0d909bee5
ferLex98 <michus.fer0503@gmail.com>
2023-03-27 18:07:07 -0500

    Multiple hooks

--- src/03-examples/MultipleCustomHooks.jsx
+++ src/03-examples/MultipleCustomHooks.jsx
@@ -2,13 +2,33 @@ import { useFetch } from "../hooks/useFetch"
 
 export const MultipleCustomHooks = () => {
 
-    const {data,isLoading,error} = useFetch('https://pokeapi.co/api/v2/pokemon/ditto')
+    const{ data,isLoading,error} = useFetch('https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0')
 
     console.log({data,isLoading,error});
+
+    const {name,url} = data;
+
+    console.log(name);
 
   return (
     <>
       <h1>Pokemon</h1>
+      <hr />
+      {
+        isLoading ?(
+            <dir className="alert alert-info text-center">
+                Loading...
+            </dir>
+        ):(
+            <blockquote className="blockquote text-end">
+                <p className="mb-1">Hola Mundo</p>
+                <footer className="blockquote-footer">Fernanda </footer>
+            </blockquote> 
+        )
+      }
+     
+
+    
     </>
   )
 }
